8cbfa55f216f62e20181403b7a115711c4a48ba9
ModestVale <visible7@mail.ru>
2021-10-26 17:29:24 +0500

    Создали .gitignore и Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer_sum = 500_000_000;
+        int total = balance + transfer_sum;
+        System.out.println(total);
+    }
+}
